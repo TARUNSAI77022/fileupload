59f0904a9b10f0f513e4b9041e8362c4f0d93b75
earbor-ai <yuvaraj4prince@gmail.com>
2023-05-10 14:34:53 +0530

    Uplaod Sample code

---
+++ FileUpload/Controllers/FileController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class FileController : Controller
+    {
+        private readonly string wwwrootDirectory =
+           Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile myFile)
+        {
+            if (myFile != null)
+            {
+                var uploadsDirectory = Path.Combine(wwwrootDirectory, "upload");
+                var path = Path.Combine(
+                   uploadsDirectory,
+                   DateTime.Now.Ticks.ToString() + Path.GetExtension(myFile.FileName));
+                using (var stream = new FileStream(path, FileMode.Create))
+                {
+                    await myFile.CopyToAsync(stream);
+                }
+
+            }
+            return View();
+        }
+    }
+}
